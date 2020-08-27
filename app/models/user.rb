53b83f5e6c4ff5bd483d5a10aaa4bd9b979ee9ed
Daniel Marino <marinodanielj@hotmail.com>
2020-08-27 19:26:17 -0400

    Added more validations for creating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   before_save { self.email = email.downcase }
-  validates :name, presence: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true,
+                   uniqueness: { case_sensitive: false },
+                   length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
   validates :password, confirmation: true
 
   has_secure_password
